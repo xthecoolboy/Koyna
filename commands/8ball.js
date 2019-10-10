65284034da9b1797402878bd4f15ca8bef8b172c
Glitch (grey-spaghetti) <none>
2019-10-10 04:27:25 +0000

    Updated with Glitch

---
+++ commands/8ball.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: '8ball',
+	description: 'Ask Bisly your questions and get quick responses.',
+  guildOnly: true,
+	aliases: ['8b'],
+  usage: '[question]',
+  cooldown: 2,
+  execute(client, message, args) {
+    	if(!args.join(" ").endsWith("?")) return message.channel.send("Ask a valid question!");
+        //if(!args[0]) return message.channel.send("Ask a vaild question!");
+        let question = args.join(" ");
+        var sayings = ["It is certain!", "It is decidedly so!", "Without a doubt!", "Yes, definitely!", "You may rely on it!", "As I see it, yes!", "Most Likely!", "Outlook good!", "Yes!", "Signs point to yes!", "Reply hazy try again!", "Ask again later!"]
+        var result = Math.floor((Math.random() * sayings.length) + 0);
+        message.channel.send("💬" + sayings[result])
+    },
+};
---
+++ commands/reboot.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'reboot',
+	description: 'Reloads the bot.',
+    guildOnly: true,
+	aliases: ['reset', 'logout'],
+    usage: '[command]',
+    cooldown: 3,
+    execute(client, message, args) {
+    		client.destroy()
+    		client.login(process.env.TOKEN)
+    	message.channel.send(`Rebooted successfully. API latency is ${Math.round(client.ping)}ms`);
+    },
+};
