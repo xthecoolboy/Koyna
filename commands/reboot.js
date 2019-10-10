e129c417576a1d7f048601b1dae1c51b233876a4
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-10-10 10:14:04 +0530

    Update reboot.js

--- commands/reboot.js
+++ commands/reboot.js
@@ -1,1 +1,14 @@
+module.exports = {
+	name: 'reboot',
+	description: 'Reloads the bot.',
+    guildOnly: true,
+	aliases: ['reset', 'logout'],
+    usage: '[command]',
+    cooldown: 3,
+    execute(client, message, args) {
+		client.destroy() /*destroying the bot login status*/
+    	client.login(process.env.TOKEN) /*logging back in into discord*/
+    	message.channel.send(`Rebooted successfully. API latency is ${Math.round(client.ping)}ms`);
+    },
+};
 
