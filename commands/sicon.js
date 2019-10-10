90ca40c0ef849b5c8c333010cebd38e7656265c4
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-10-10 10:16:11 +0530

    Update sicon.js

--- commands/sicon.js
+++ commands/sicon.js
@@ -3,13 +3,13 @@ const { RichEmbed } = require('discord.js');
 module.exports = {
 	name: 'sicon',
 	description: 'Get the guild/server icon.',
-  guildOnly: true,
+  	guildOnly: true,
 	aliases: ['servericon', 'icon'],
-  usage: '[command]',
-  cooldown: 5,
-  execute(client, message, args) {
+  	usage: '[command]',
+  	cooldown: 5,
+  	execute(client, message, args) {
     	let embed = new RichEmbed()
-      .setColor("#000000")
+      	.setColor("#000000")
     	.setImage(message.guild.iconURL + '?size=2048')
     	.setFooter(message.guild.name)
     	message.channel.send(embed)
