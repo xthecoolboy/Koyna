496ac6ca6d4b2f9a154f4f8f5612aef9aa6f798a
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-10-23 23:16:36 +0530

    Update reboot.js
    updated reboot command with more flexible code

--- commands/reboot.js
+++ commands/reboot.js
@@ -1,13 +1,17 @@
+const token = process.env.TOKEN;
+const id = process.env.OwnerID;
+
 module.exports = {
 	name: 'reboot',
 	description: 'Reloads the bot.',
     guildOnly: true,
 	aliases: ['reset', 'logout'],
-    usage: '[command]',
+    usage: '',
     cooldown: 3,
     execute(client, message, args) {
-		client.destroy() /*destroying the bot login status*/
-    	client.login(process.env.TOKEN) /*logging back in into discord*/
+		if (!message.author.id == id) return message.channel.send(`You can't use this command!`);
+		client.destroy() //destroying the bot login status
+    	client.login(process.env.TOKEN) //logging back in into discord
     	message.channel.send(`Rebooted successfully. API latency is ${Math.round(client.ping)}ms`);
     },
 };
