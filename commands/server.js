af60855f04b0e93313c46f6088f5305d174cf7ca
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-11-03 22:46:18 +0530

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -9,25 +9,26 @@ module.exports = {
 	cooldown: 3,
 	execute(client, message, args) {
 		//server categories
-		let chnlCat = msg.guild.channels.map(c => {
+		let chnlCat = message.guild.channels.map(c => {
       return `${c.type == 'text' || c.type == 'voice'? '': c.name}`
     }).filter(c => c != '').sort();
 		
 		//server channels
-		let textChnl = msg.guild.channels.map(c => {
+		let textChnl = message.guild.channels.map(c => {
       return `${c.type == 'category' || c.type == 'voice'? '': c.name}`
     }).filter(c => c != '').sort();
 		
 		//voice channels
-		let voiceChnl = msg.guild.channels.map(c => {
+		let voiceChnl = message.guild.channels.map(c => {
       return `${c.type == 'category' || c.type == 'text'? '': c.name}`
     }).filter(c => c != '').sort();
 		
-		var afkChannel;
-		if(message.guild.afkChannel === null) {
-			afkChannel == 'None';
-		} else {
-			afkChannel == `${message.guild.afkChannel}`;
+		//var afkChannel;
+		//if(message.guild.afkChannel === null) {
+			//afkChannel == 'None';
+		//} else {
+			//afkChannel == `${message.guild.afkChannel}`;
+    //}
 		
 		let embed = new RichEmbed()
     	.setColor('RANDOM') // Random color everytime
@@ -35,19 +36,18 @@ module.exports = {
     	.setThumbnail(message.guild.iconURL)
     	//.addField("ServerName:", message.guild.name)
     	//.addField("ServerID:", message.guild.id)
-			.addField(`**Roles:`, message.guild.roles.map(r => r.name).sort().splice(1).join(', '))
-			.addField(`Channels:` `> **${textChnl.length}** Text, **${voiceChnl.length}** Voice, **${chnlCat.length}** Categories.\n
-				> AFK: **${afkChannel}**`)
-			.addField(`Members:`, `> ${message.guild.memberCount} members\n 
-				> Owner: **${message.guild.owner}#${message.guild.owner.discriminator}**\n
-				(ID: **${message.guild.owner.id}**`)
+			.addField(`**Roles**`, message.guild.roles.map(r => r.name).sort().splice(1).join(', '))
+			.addField(`Channels:`, `> **${textChnl.length}** Text, **${voiceChnl.length}** Voice, **${chnlCat.length}** Categories.
+				> AFK: **${message.guild.afkChannel}**`)
+			.addField(`Members:`, `> ${message.guild.memberCount} members
+				> Owner: **${message.guild.owner}** (ID: **${message.guild.owner.id}**)`, true)
     	//.addField("CreatedAt:", message.guild.createdAt)
     	//.addField("Owner:", message.guild.owner)
     	//.addField("OwnerId:", message.guild.owner.id)
     	//.addField("MemberCount:", message.guild.memberCount)
-			.addField(`Other:`, `> Roles: **${message.guild.roles.size}**\n
-				> Region: **${message.guild.region.slice(0,1).toUpperCase()}${message.guild.region.slice(1)}**\n
-				> Created at: **${message.guild.createdAt}**\n
+			.addField(`Other:`, `> Roles: **${message.guild.roles.size}**
+				> Region: **${message.guild.region.slice(0,1).toUpperCase()}${message.guild.region.slice(1)}**
+				> Created at: **${message.guild.createdAt}**
 				> Verification Level: **${message.guild.verificationLevel}**`)
     	//.addField("VerificationLevel:", message.guild.verificationLevel)
     	//.addField("Region:", `${message.guild.region.slice(0,1).toUpperCase()}${message.guild.region.slice(1)}`)
