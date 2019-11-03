f20ebd05999a49cf340ea18bad7b4afca38bf710
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-11-03 22:12:48 +0530

    updated server.js

--- commands/server.js
+++ commands/server.js
@@ -8,6 +8,27 @@ module.exports = {
 	usage: '[command]',
 	cooldown: 3,
 	execute(client, message, args) {
+		//server categories
+		let chnlCat = msg.guild.channels.map(c => {
+      return `${c.type == 'text' || c.type == 'voice'? '': c.name}`
+    }).filter(c => c != '').sort();
+		
+		//server channels
+		let textChnl = msg.guild.channels.map(c => {
+      return `${c.type == 'category' || c.type == 'voice'? '': c.name}`
+    }).filter(c => c != '').sort();
+		
+		//voice channels
+		let voiceChnl = msg.guild.channels.map(c => {
+      return `${c.type == 'category' || c.type == 'text'? '': c.name}`
+    }).filter(c => c != '').sort();
+		
+		var afkChannel;
+		if(message.guild.afkChannel === null) {
+			afkChannel == 'None';
+		} else {
+			afkChannel == `${message.guild.afkChannel}`;
+		
 		let embed = new RichEmbed()
     	.setColor('RANDOM') // Random color everytime
     	.setTitle(`**${message.guild.name} [${message.guild.id}]**`) 
@@ -15,14 +36,23 @@ module.exports = {
     	//.addField("ServerName:", message.guild.name)
     	//.addField("ServerID:", message.guild.id)
 			.addField(`**Roles:`, message.guild.roles.map(r => r.name).sort().splice(1).join(', '))
-    	.addField("CreatedAt:", message.guild.createdAt)
-    	.addField("Owner:", message.guild.owner)
-    	.addField("OwnerId:", message.guild.owner.id)
-    	.addField("MemberCount:", message.guild.memberCount)
-    	.addField("VerificationLevel:", message.guild.verificationLevel)
-    	.addField("Region:", `${message.guild.region.slice(0,1).toUpperCase()}${message.guild.region.slice(1)}`)
-    	.setTimestamp(new Date())
-    	.setFooter(`Requested by: ${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL)
+			.addField(`Channels:` `> **${textChnl.length}** Text, **${voiceChnl.length}** Voice, **${chnlCat.length}** Categories.\n
+				> AFK: **${afkChannel}**`)
+			.addField(`Members:`, `> ${message.guild.memberCount} members\n 
+				> Owner: **${message.guild.owner}#${message.guild.owner.discriminator}**\n
+				(ID: **${message.guild.owner.id}**`)
+    	//.addField("CreatedAt:", message.guild.createdAt)
+    	//.addField("Owner:", message.guild.owner)
+    	//.addField("OwnerId:", message.guild.owner.id)
+    	//.addField("MemberCount:", message.guild.memberCount)
+			.addField(`Other:`, `> Roles: **${message.guild.roles.size}**\n
+				> Region: **${message.guild.region.slice(0,1).toUpperCase()}${message.guild.region.slice(1)}**\n
+				> Created at: **${message.guild.createdAt}**\n
+				> Verification Level: **${message.guild.verificationLevel}**`)
+    	//.addField("VerificationLevel:", message.guild.verificationLevel)
+    	//.addField("Region:", `${message.guild.region.slice(0,1).toUpperCase()}${message.guild.region.slice(1)}`)
+    	//.setTimestamp(new Date())
+    	//.setFooter(`Requested by: ${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL)
     	message.channel.send(embed)
 	},
 };
