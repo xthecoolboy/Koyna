4972d5d56eff16ec3785dac96874b2cee9ca8d1b
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-11-04 18:14:54 +0530

    updated server.js
    removed all code comments, including irrelevant old code which was updated yesterday

--- commands/server.js
+++ commands/server.js
@@ -23,36 +23,19 @@ module.exports = {
       return `${c.type == 'category' || c.type == 'text'? '': c.name}`
     }).filter(c => c != '').sort();
 		
-		//var afkChannel;
-		//if(message.guild.afkChannel === null) {
-			//afkChannel == 'None';
-		//} else {
-			//afkChannel == `${message.guild.afkChannel}`;
-    //}
-		
 		let embed = new RichEmbed()
-    	.setColor('#20bab5') // Random color everytime
+    	.setColor('#20bab5') 
     	.setTitle(`**${message.guild.name} [${message.guild.id}]**`) 
     	.setThumbnail(message.guild.iconURL)
-    	//.addField("ServerName:", message.guild.name)
-    	//.addField("ServerID:", message.guild.id)
 			.addField(`**Roles**`, message.guild.roles.map(r => r.name).sort().splice(1).join(', '))
 			.addField(`Channels:`, `> **${textChnl.length}** Text, **${voiceChnl.length}** Voice, **${chnlCat.length}** Categories.
 				> AFK: **${message.guild.afkChannel}**`)
 			.addField(`Members:`, `> ${message.guild.memberCount} members
 				> Owner: **${message.guild.owner}** (ID: **${message.guild.owner.id}**)`)
-    	//.addField("CreatedAt:", message.guild.createdAt)
-    	//.addField("Owner:", message.guild.owner)
-    	//.addField("OwnerId:", message.guild.owner.id)
-    	//.addField("MemberCount:", message.guild.memberCount)
 			.addField(`Other:`, `> Roles: **${message.guild.roles.size}**
 				> Region: **${message.guild.region.slice(0,1).toUpperCase()}${message.guild.region.slice(1)}**
 				> Created at: **${message.guild.createdAt}**
 				> Verification Level: **${message.guild.verificationLevel}**`)
-    	//.addField("VerificationLevel:", message.guild.verificationLevel)
-    	//.addField("Region:", `${message.guild.region.slice(0,1).toUpperCase()}${message.guild.region.slice(1)}`)
-    	//.setTimestamp(new Date())
-    	//.setFooter(`Requested by: ${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL)
     	message.channel.send(embed)
 	},
 };
