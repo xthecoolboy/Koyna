7ca462afe26e24cb732f8f4929fe09b60ba46c65
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-11-10 10:59:45 +0530

    Update virgin.js

--- commands/virgin.js
+++ commands/virgin.js
@@ -1,15 +1,21 @@
+const { RichEmbed } = require('discord.js');
+
 module.exports = {
     name: 'virgin',
-    description: 'Check you virginity.',
+    description: 'Random virginity calculator.',
     guildOnly : true,
     usage: '[mention user]',
     cooldown: 2,
     execute(client, message, args) {
       let User = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    	if(!User) return message.channel.send('Mention a user');
+      if(!User) return message.channel.send('Mention a user!');
       //if(!args[0]) return message.channel.send("Ask a vaild question!");
-      var sayings = ["is a virgin", "is not a virgin", "is neutral"]
+      var sayings = [" is a virgin", " is not a virgin", " is neutral"]
       var result = Math.floor((Math.random() * sayings.length) + 0);
-      message.channel.send(User + sayings[result])
+      let embed = new RichEmbed()
+      .setTitle('Virginity Test')
+      .setColor('#ed2c09')
+      .setDescription(User + sayings[result])
+      message.channel.send(embed)
     },
 };
