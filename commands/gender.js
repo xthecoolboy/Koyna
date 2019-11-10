559a54e9ea7f7c2f85ed8b8fb6a923dd750bd246
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-11-10 11:07:36 +0530

    Create gender.js

---
+++ commands/gender.js
@@ -0,0 +1,21 @@
+const { RichEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'gender',
+    description: 'Random gender calculator.',
+    guildOnly : true,
+    usage: '[mention user]',
+    cooldown: 2,
+    execute(client, message, args) {
+      let User = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+      if(!User) return message.channel.send('Mention a user!');
+      //if(!args[0]) return message.channel.send("Ask a vaild question!");
+      var sayings = [" is male", " is not a female", " is lesbian", " is gay", " is shemale", " is neutral"]
+      var result = Math.floor((Math.random() * sayings.length) + 0);
+      let embed = new RichEmbed()
+      .setTitle('Gender Test')
+      .setColor('#ed2c09')
+      .setDescription(User + sayings[result])
+      message.channel.send(embed)
+    },
+};
