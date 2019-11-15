640315e9f58f5ee8d2e00a65d2e67d7707de24ea
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-11-15 23:20:20 +0530

    updated stats.js
    updated the time formant

--- commands/stats.js
+++ commands/stats.js
@@ -1,5 +1,6 @@
 const { RichEmbed } = require('discord.js');
 const os = require('os');
+const moment = require('moment');
 
 module.exports = {
 	name: 'stats',
@@ -40,11 +41,14 @@ module.exports = {
     let client_uptime = new Convert(client.uptime / 1000).ToDHMS();
     let os_uptime = new Convert(os.uptime()).ToDHMS();
     let process_uptime = new Convert(process.uptime()).ToDHMS(); 
+		
+		let createdAt = moment(client.user.createdAt);
+		let create = createdAt.format('MMMM Do YYYY, h:mm:ss a');
 
     let embed = new RichEmbed()
     .setColor('#F70827')
     .setTitle("Stats")
-    .addField("Koyna", `**CreatedAt: **${client.user.createdAt}\n**Guilds: **${client.guilds.size}\n**Channels: **${client.channels.size}\n**Users: **${client.guilds.reduce((a, b) => a + b.memberCount, 0)}\n**Discord.js: **v11.5.1\n**Node.js: **${process.version}`)
+    .addField("Koyna", `**CreatedAt: **${create}\n**Guilds: **${client.guilds.size}\n**Channels: **${client.channels.size}\n**Users: **${client.guilds.reduce((a, b) => a + b.memberCount, 0)}\n**Discord.js: **v11.5.1\n**Node.js: **${process.version}`)
     .addField("Uptime", `**Client: **${client_uptime}\n**Host OS: **${os_uptime}\n**Process: **${process_uptime}`)
     .addField("Ram", `**Total: **${ram_total}\n**Usage: **${ram_usage}`)
     message.channel.send(embed)
