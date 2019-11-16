e4378574935bd9a07670a6e4552e91e8f0449dfa
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-11-16 15:18:18 +0530

    updated imdb.js
    fixed several bugs and errors

--- commands/imdb.js
+++ commands/imdb.js
@@ -4,29 +4,29 @@ const imdb = require('imdb');
 
 module.exports = {
   name: 'imdb',
-  description: 'Get info about Movies',
+  description: 'Get basic info about Movies and shows',
   guildOnly: true,
   aliases: ['movie'],
   usage: 'string',
   cooldown: 3,
   execute(client, message, args) {
-    if (!args) return message.channel.send('Enter a movie to search for!');
     let movie = args.join(' ')
+    if(!movie) return message.channel.send('Enter a movie to search for!');
     nameToImdb({name: movie}, function(err, res, inf){
+      if(res == null) return message.channel.send("Can't find that movie!");
       let movie_id = res;
       console.log(res);
       imdb(movie_id, function(err, data) {
         if (data)
         var embed;
         embed = new RichEmbed()
+        .setColor('#fcba03')
         .setTitle(data.title)
         .setThumbnail(data.poster)
         .setDescription(`**Description:** ${data.description}\n**Rating:** ${data.rating}\n**Genre/Release:** ${data.genre}`)
         .setFooter(`Metascore: ${data.metascore}`)
         .setTimestamp(new Date())
         message.channel.send(embed)
-        if (err)
-          message.channel.send(`Can't find that movie!`);
         });
       });
   },
