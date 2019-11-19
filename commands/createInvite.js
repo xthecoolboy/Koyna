bca4c5c6c1b6221ddb264a296ea44fb46ebe3a1c
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-11-19 20:27:17 +0530

    Update createInvite.js

--- commands/createInvite.js
+++ commands/createInvite.js
@@ -3,13 +3,13 @@ module.exports = {
 	description: 'Create a temporary server invite.',
   guildOnly: true,
 	aliases: ['ci'],
-  usage: ['command.', 'Maximum number of uses: 3, Appx time: 17 minutes'],
+  usage: ['command, Maximum number of uses: 3, Appx time: 17 minutes'],
 	cooldown: 2,
 	async execute(client, message, args) {
     let invite = await message.channel.createInvite({
-    maxAge: 10 * 60 * 1000000, //maximum time for the invite, in milliseconds
+    maxAge: 1000, //maximum time for the invite, in milliseconds
     maxUses: 3 //maximum times it can be used
     });
-    message.reply(invite ? `Here's your invite: ${invite} : "There has been an error during the creation of the invite."`);
+    message.reply(`${invite ? "Here is you invite", invite : "Sorry, can't generate the invite"}`);
 	},
 };
